425b24285ade1d6ef49bf7522b358630fb2ef576
王泳淇 <815099246@qq.com>
2023-08-08 15:59:39 +0800

    commit

--- plugin/src/main/java/io/ecosed/plugin/EcosedPluginBinding.kt
+++ plugin/src/main/java/io/ecosed/plugin/EcosedPluginBinding.kt
@@ -2,10 +2,20 @@ package io.ecosed.plugin
 
 import android.app.Activity
 
+/**
+ * 作者: wyq0918dev
+ * 仓库: https://github.com/ecosed/EcosedPlugin
+ * 时间: 2023/08/08
+ * 描述: 插件绑定器
+ * 文档: https://github.com/ecosed/EcosedPlugin/blob/master/README.md
+ */
 class EcosedPluginBinding(activity: Activity) {
 
     private val mActivity: Activity = activity
 
+    /**
+     * 内部方法：获取Activity
+     */
     internal fun getActivity(): Activity {
         return mActivity
     }
--- plugin/src/main/java/io/ecosed/plugin/EcosedPluginMethod.kt
+++ plugin/src/main/java/io/ecosed/plugin/EcosedPluginMethod.kt
@@ -2,6 +2,13 @@ package io.ecosed.plugin
 
 import android.app.Activity
 
+/**
+ * 作者: wyq0918dev
+ * 仓库: https://github.com/ecosed/EcosedPlugin
+ * 时间: 2023/08/08
+ * 描述: 插件方法
+ * 文档: https://github.com/ecosed/EcosedPlugin/blob/master/README.md
+ */
 class EcosedPluginMethod(binding: EcosedPluginBinding, channel: String) {
 
     private var mBinding: EcosedPluginBinding = binding
@@ -9,20 +16,38 @@ class EcosedPluginMethod(binding: EcosedPluginBinding, channel: String) {
     private lateinit var mCallBack: EcosedMethodCallHandler
     private var mResult: Any? = null
 
+    /**
+     * 设置方法调用
+     * @param callBack 执行方法时调用EcosedMethodCallHandler
+     */
     fun setMethodCallHandler(callBack: EcosedMethodCallHandler?) {
         callBack?.let {
             this.mCallBack = it
         }
     }
 
+    /**
+     * 获取Activity
+     * @return 插件附加的Activity
+     */
     fun getActivity(): Activity {
         return mBinding.getActivity()
     }
 
+    /**
+     * 获取通道
+     * @return 通道名称
+     */
     fun getChannel() :String {
         return mChannel
     }
 
+    /**
+     * 执行方法回调
+     * @param channel 通道名称
+     * @param call 方法名称
+     * @return 方法执行后的返回值
+     */
     fun execMethodCall(channel: String, call: String?): Any? {
         if (channel == mChannel){
             call?.let {
@@ -37,7 +62,6 @@ class EcosedPluginMethod(binding: EcosedPluginBinding, channel: String) {
                 })
             }
         }
-
         return mResult
     }
 }
